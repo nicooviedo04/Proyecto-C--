5e2b7741d4f91f2af4b49699762bed95ae8a08b8
Nicolás <Nicoovi@gmail.com>
2025-09-05 23:05:51 -0300

    Primer HOLA MUNDO

---
+++ cursoC++.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream> //libreria que permite entrada y salida de datos
+
+int main(){
+    
+    
+    std::cout << "Hola Mundo" << std::endl; //imprime en pantalla el mensaje Hola Mundo
+
+
+
+    return 0;
+
+}
+
+
+
